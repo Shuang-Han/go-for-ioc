3897972536cc05b4be36c914b2125ee2c8c017f2
JustBanMe <brandon.p.gan@yzw.cn>
2020-12-28 17:56:21 +0800

    update

--- thinking-in-spring/ioc-container-overview/src/main/java/com/ganpengyu/study/ioc/dependency/injection/DependencyInjection.java
+++ thinking-in-spring/ioc-container-overview/src/main/java/com/ganpengyu/study/ioc/dependency/injection/DependencyInjection.java
@@ -3,12 +3,22 @@ package com.ganpengyu.study.ioc.dependency.injection;
 import com.ganpengyu.study.ioc.repository.UserRepository;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.Environment;
 
 public class DependencyInjection {
 
     public static void main(String[] args) {
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-injection-context.xml");
+
         UserRepository userRepository = beanFactory.getBean(UserRepository.class);
-        System.out.println(userRepository.getUsers());
+
+        System.out.println(userRepository.getBeanFactory());
+        System.out.println(userRepository.getBeanFactory() == beanFactory);
+
+        System.out.println(userRepository.getObjectFactory());
+        System.out.println(userRepository.getObjectFactory().getObject() == beanFactory);
+
+        Environment environment = beanFactory.getBean(Environment.class);
+        System.out.println(environment);
     }
 }
--- thinking-in-spring/ioc-container-overview/src/main/java/com/ganpengyu/study/ioc/repository/UserRepository.java
+++ thinking-in-spring/ioc-container-overview/src/main/java/com/ganpengyu/study/ioc/repository/UserRepository.java
@@ -1,12 +1,35 @@
 package com.ganpengyu.study.ioc.repository;
 
 import com.ganpengyu.study.ioc.domain.User;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.context.ApplicationContext;
 
 import java.util.Collection;
 
 public class UserRepository {
 
     private Collection<User> users;
+
+    private BeanFactory beanFactory;
+
+    private ObjectFactory<ApplicationContext> objectFactory;
+
+    public ObjectFactory<ApplicationContext> getObjectFactory() {
+        return objectFactory;
+    }
+
+    public void setObjectFactory(ObjectFactory<ApplicationContext> objectFactory) {
+        this.objectFactory = objectFactory;
+    }
+
+    public BeanFactory getBeanFactory() {
+        return beanFactory;
+    }
+
+    public void setBeanFactory(BeanFactory beanFactory) {
+        this.beanFactory = beanFactory;
+    }
 
     public Collection<User> getUsers() {
         return users;
